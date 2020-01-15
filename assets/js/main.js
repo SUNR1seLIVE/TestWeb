dd76b2dd30fcec6f4a4127d2004c62462b61d97a
Alexandr Yaroslav <Sunrisefixtures@gmail.com>
2020-01-15 08:58:49 +0300

    15.01.2020 8:57
    Закончил работу над меню.
    Задачу решил и можно объеденить с основным проектом.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    var triger = $('.triger .li-container');
+    var clonlimin = $('.li-min').clone();
+    var clonlimax = $('.li-max').clone();
 
-    var $clonlimin = $('.li-min-none').clone();
-    var $clonlimax = $('.li-max-none').clone();
 
-    $('btn').on('click',function(){
-        alert( $clonlimax );
-    })
+    if ($(window).width() > 991) {
+        $('.li-max').css('display', 'list-item');
+        triger.empty();
+        triger.prepend(clonlimin);
+        var count_li = $(triger).children().length;
+        $('.count-li').text(count_li);
+    } else {
+        $('.li-max').css('display', 'none');
+        triger.empty();
+        triger.prepend(clonlimin);
+        triger.prepend(clonlimax);
+        $('.li-container .li-max').css('display', 'list-item');
+        var count_li = $(triger).children().length;
+        $('.count-li').text(count_li);
+    }
 
-    // $('.min-triger .li-container').prepend( $clonlimin );
-    // $('.max-triger .li-container').prepend( $clonlimin );
+    $(window).on('resize', function () {
+        if ($(window).width() > 991) {
+            $('.li-max').css('display', 'list-item');
+            triger.empty();
+            triger.prepend(clonlimin);
+            var count_li = $(triger).children().length;
+            $('.count-li').text(count_li);
+        } else {
+            $('.li-max').css('display', 'none');
+            triger.empty();
+            triger.prepend(clonlimin);
+            triger.prepend(clonlimax);
+            $('.li-container .li-max').css('display', 'list-item');
+            var count_li = $(triger).children().length;
+            $('.count-li').text(count_li);
+        }
+    });
 
-    
+
+
+
+
 
 });
